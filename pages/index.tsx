4924d3f557c51ac264416bdfa7c3ef29f06fbe6b
nammulovett <nammmu@gmail.com>
2022-11-19 21:02:42 +0100

    -

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,53 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Heading,
+  StackDivider,
+  Stack,
+  Box,
+} from '@chakra-ui/react';
 
 export default function Home() {
-  return <p>Corita rules</p>;
+  return (
+    <Card>
+      <CardHeader>
+        <Heading size='md'>Client Report</Heading>
+      </CardHeader>
+
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing='4'>
+          <Box>
+            <Heading size='xs' textTransform='uppercase'>
+              Summary
+            </Heading>
+            <Text pt='2' fontSize='sm'>
+              View a summary of all your clients over the last month.
+            </Text>
+          </Box>
+          <Box>
+            <Heading size='xs' textTransform='uppercase'>
+              Overview
+            </Heading>
+            <Text pt='2' fontSize='sm'>
+              Check out the overview of your clients.
+            </Text>
+          </Box>
+          <Box>
+            <Heading size='xs' textTransform='uppercase'>
+              Analysis
+            </Heading>
+            <Text pt='2' fontSize='sm'>
+              See a detailed analysis of all your business clients.
+            </Text>
+          </Box>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
 }
